35a7db3c578347b1e92e2972166b0c1280d5e1a5
Borovichki <sniper12459@gmail.com>
2024-06-14 21:30:11 +0400

    initial commit

---
+++ database/migrations/2023_12_19_165401_create_todos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->longText('title');
+            $table->boolean('is_completed')->default(0);
+            $table->timestamps();
+            $table->integer('user_id')->unsigned()->index()->nullable();
+            // $table->foreign('user_id')->references('id')->on('users')->ondelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('todos');
+    }
+};
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Tasklist') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <!-- <x-welcome /> -->
+                <a href="{{ url('/todos') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500" wire:navigate="">Tasklist</a>
+
+            </div>
+        </div>
+    </div>
+</x-app-layout>
